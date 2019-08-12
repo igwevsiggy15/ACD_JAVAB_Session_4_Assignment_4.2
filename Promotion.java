2d2957dd1e4a7ab568c61d18a2c77c81d6956f95
igwevsiggy15 <53847461+igwevsiggy15@users.noreply.github.com>
2019-08-12 14:30:13 -0400

    Add files via upload

---
+++ Promotion.java
@@ -0,0 +1,11 @@
+package Assignment2;
+
+public class Promotion {
+
+	public static void main(String[] args) {
+		int i = 7;
+		System.out.println("Integer: " + i);
+		float l = i;
+		System.out.println("float l = i: " + l);
+	}
+}
